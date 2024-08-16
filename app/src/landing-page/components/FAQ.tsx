0689ba277f4e5d9af4211b188975068d6f47d29c
William Flanders <wflan@seas.upenn.edu>
2024-08-16 09:32:39 -0400

    create FAQ component

--- app/src/landing-page/components/FAQ.tsx
+++ app/src/landing-page/components/FAQ.tsx
@@ -1,33 +1,50 @@
+import React, { useState, useRef } from 'react';
 interface FAQ {
   id: number;
   question: string;
   answer: string;
-  href?: string;
 };
 
 export default function FAQ({ faqs }: { faqs: FAQ[] }) {
+  const [answerVisible, setAnswerVisible] = useState(Array(faqs.length).fill(false));
+  const answersRef = useRef(new Array(faqs.length));  // Adjust ref array initialization
+
+  // Function to toggle the visibility of an FAQ answer
+  const toggleAnswer = (index: number): void => {
+    setAnswerVisible(current => {
+      const newVisibility = [...current];
+      newVisibility[index] = !newVisibility[index];  // Toggle the state for the specific FAQ
+      return newVisibility;
+    });
+  };
   return (
-    <div className='mt-32 mx-auto max-w-2xl divide-y divide-gray-900/10 dark:divide-gray-200/10 px-6 pb-8 sm:pb-24 sm:pt-12 lg:max-w-7xl lg:px-8 lg:py-32'>
-      <h2 className='text-2xl font-bold leading-10 tracking-tight text-gray-900 dark:text-white'>
-        Frequently asked questions
-      </h2>
-      <dl className='mt-10 space-y-8 divide-y divide-gray-900/10'>
-        {faqs.map((faq) => (
-          <div key={faq.id} className='pt-8 lg:grid lg:grid-cols-12 lg:gap-8'>
-            <dt className='text-base font-semibold leading-7 text-gray-900 lg:col-span-5 dark:text-white'>
-              {faq.question}
-            </dt>
-            <dd className='flex items-center justify-start gap-2 mt-4 lg:col-span-7 lg:mt-0'>
-              <p className='text-base leading-7 text-gray-600 dark:text-white'>{faq.answer}</p>
-              {faq.href && (
-                <a href={faq.href} className='text-base leading-7 text-yellow-500 hover:text-yellow-600'>
-                  Learn more →
-                </a>
-              )}
-            </dd>
-          </div>
-        ))}
-      </dl>
+    <div className='flex justify-center items-center'>
+      <div className='mt-32 mb-32 mx-auto max-w-2xl divide-y divide-gray-900/10 dark:divide-gray-200/10 px-6 pb-8 sm:pb-24 sm:pt-12 lg:max-w-7xl lg:px-8 lg:py-32'>
+        <h2 className='text-2xl font-bold text-center leading-10 tracking-tight text-gray-900 dark:text-white'>
+          FAQs
+        </h2>
+        <dl className='mt-10 space-y-8 divide-y divide-gray-900/10'>
+          {faqs.map((faq, index) => (
+            <div key={faq.id} className='pt-8'>
+              <dt className='text-base font-semibold leading-7 text-gray-900 dark:text-white'>
+                <button 
+                  type="button" 
+                  className="flex items-center justify-center gap-2 w-full text-left"
+                  onClick={() => toggleAnswer(index)}
+                >
+                  {faq.question}
+                </button>
+              </dt>
+              <dd className={`mt-2 overflow-hidden text-center transition-height duration-500 ease-in-out`}
+                  ref={el => answersRef.current[index] = el}
+                  style={{ height: answerVisible[index] ? `${answersRef.current[index]?.scrollHeight}px` : '0px' }}
+              >
+                <p className='text-base leading-7 text-gray-600 dark:text-white'>{faq.answer}</p>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
     </div>
   )
 }
