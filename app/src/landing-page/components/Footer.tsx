a18c0b6aa638163209dc6401174a6f4acb6a02c4
William Flanders <wflan@seas.upenn.edu>
2024-08-22 11:34:48 -0400

    footer component

--- app/src/landing-page/components/Footer.tsx
+++ app/src/landing-page/components/Footer.tsx
@@ -10,41 +10,41 @@ export default function Footer({ footerNavigation }: {
   }
 }) {
   return (
-    <div className='mx-auto mt-6 max-w-7xl px-6 lg:px-8 dark:bg-boxdark-2'>
+    <div className='-mt-2 mx-auto dark:bg-boxdark-2'>
       <footer
-        aria-labelledby='footer-heading'
-        className='relative border-t border-gray-900/10 dark:border-gray-200/10 py-24 sm:mt-32'
-      >
-        <h2 id='footer-heading' className='sr-only'>
-          Footer
-        </h2>
-        <div className='flex items-start justify-end mt-10 gap-20'>
-          <div>
-            <h3 className='text-sm font-semibold leading-6 text-gray-900 dark:text-white'>App</h3>
-            <ul role='list' className='mt-6 space-y-4'>
-              {footerNavigation.app.map((item) => (
-                <li key={item.name}>
-                  <a href={item.href} className='text-sm leading-6 text-gray-600 hover:text-gray-900 dark:text-white'>
-                    {item.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
+          aria-labelledby='footer-heading'
+          className='relative bg-rldark py-24'
+        >
+          <h2 id='footer-heading' className='sr-only'>
+            Footer
+          </h2>
+          <div className='flex items-start justify-start ml-10 mt-10 gap-20'>
+            <div>
+              <h3 className='text-sm font-semibold leading-6 text-gray-300'>App</h3>
+              <ul role='list' className='mt-6 space-y-4'>
+                {footerNavigation.app.map((item) => (
+                  <li key={item.name}>
+                    <a href={item.href} className='text-sm leading-6 text-white hover:text-gray-300'>
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-sm font-semibold leading-6 text-gray-300'>©2024 RAFFLE LEADER, INC.</h3>
+              <ul role='list' className='mt-6 space-y-4'>
+                {footerNavigation.company.map((item) => (
+                  <li key={item.name}>
+                    <a href={item.href} className='text-sm leading-6 text-white hover:text-gray-300'>
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div>
-            <h3 className='text-sm font-semibold leading-6 text-gray-900 dark:text-white'>Company</h3>
-            <ul role='list' className='mt-6 space-y-4'>
-              {footerNavigation.company.map((item) => (
-                <li key={item.name}>
-                  <a href={item.href} className='text-sm leading-6 text-gray-600 hover:text-gray-900 dark:text-white'>
-                    {item.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </footer>
+        </footer>
     </div>
   )
 }
