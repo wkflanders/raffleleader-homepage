c0c47a0ed10c4ef3138f5059ed0d9d63f2cfd7bf
William Flanders <wflan@seas.upenn.edu>
2024-08-22 11:35:24 -0400

    preliminary pricing page fixes

--- app/src/payment/PricingPage.tsx
+++ app/src/payment/PricingPage.tsx
@@ -7,7 +7,7 @@ import { useHistory } from 'react-router-dom';
 import { cn } from '../client/cn';
 import { z } from 'zod';
 
-const bestDealPaymentPlanId: PaymentPlanId = PaymentPlanId.Pro;
+const bestDealPaymentPlanId: PaymentPlanId = PaymentPlanId.Yearly;
 
 interface PaymentPlanCard {
   name: string;
@@ -17,24 +17,18 @@ interface PaymentPlanCard {
 };
 
 export const paymentPlanCards: Record<PaymentPlanId, PaymentPlanCard> = {
-  [PaymentPlanId.Hobby]: {
-    name: prettyPaymentPlanName(PaymentPlanId.Hobby),
-    price: '$9.99',
-    description: 'All you need to get started',
-    features: ['Limited monthly usage', 'Basic support'],
+  [PaymentPlanId.Yearly]: {
+    name: prettyPaymentPlanName(PaymentPlanId.Yearly),
+    price: '$97',
+    description: '$197',
+    features: ['Unlimited Sites', 'Pre-Built Templates', 'Customizeable Giveaways', 'Email and Social Media Integrations', '14-day Money-Back Guarantee'],
   },
-  [PaymentPlanId.Pro]: {
-    name: prettyPaymentPlanName(PaymentPlanId.Pro),
-    price: '$19.99',
-    description: 'Our most popular plan',
-    features: ['Unlimited monthly usage', 'Priority customer support'],
+  [PaymentPlanId.Lifetime]: {
+    name: prettyPaymentPlanName(PaymentPlanId.Lifetime),
+    price: '$397',
+    description: '$497',
+    features: ['Unlimited Sites', 'Lifetime Updates', 'Lifetime Support', 'One-Time Payment', 'No Raffle Leader Branding'],
   },
-  [PaymentPlanId.Credits10]: {
-    name: prettyPaymentPlanName(PaymentPlanId.Credits10),
-    price: '$9.99',
-    description: 'One-time purchase of 10 credits for your account',
-    features: ['Use credits for e.g. OpenAI API calls', 'No expiration date'],
-  }
 };
 
 const PricingPage = () => {
@@ -81,8 +75,8 @@ const PricingPage = () => {
     <div className='py-10 lg:mt-10'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div id='pricing' className='mx-auto max-w-4xl text-center'>
-          <h2 className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white'>
-            Pick your <span className='text-yellow-500'>pricing</span>
+          <h2 className='mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white'>
+            Get <span className='text-raffleleader'>Raffle Leader</span> Now
           </h2>
         </div>
         <p className='mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-white'>
@@ -90,12 +84,12 @@ const PricingPage = () => {
           test credit card number{' '}
           <span className='px-2 py-1 bg-gray-100 rounded-md text-gray-500'>4242 4242 4242 4242 4242</span>
         </p>
-        <div className='isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 lg:gap-x-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+        <div className='isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 lg:gap-x-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
           {Object.values(PaymentPlanId).map((planId) => (
             <div
               key={planId}
               className={cn(
-                'relative flex flex-col grow justify-between rounded-3xl ring-gray-900/10 dark:ring-gray-100/10 overflow-hidden p-8 xl:p-10',
+                'relative flex flex-col grow justify-between rounded-3xl ring-gray-900/10 dark:ring-gray-100/10 overflow-hidden p-8 xl:p-10 border-slate-700 border-2',
                 {
                   'ring-2': planId === bestDealPaymentPlanId,
                   'ring-1 lg:mt-8': planId !== bestDealPaymentPlanId,
@@ -105,7 +99,7 @@ const PricingPage = () => {
               {planId === bestDealPaymentPlanId && (
                 <div className='absolute top-0 right-0 -z-10 w-full h-full transform-gpu blur-3xl' aria-hidden='true'>
                   <div
-                    className='absolute w-full h-full bg-gradient-to-br from-amber-400 to-purple-300 opacity-30 dark:opacity-50'
+                    className='absolute w-full h-full bg-gradient-to-b from-raffleleader to-violet-400 opacity-15'
                     style={{
                       clipPath: 'circle(670% at 50% 50%)',
                     }}
@@ -114,11 +108,11 @@ const PricingPage = () => {
               )}
               <div className='mb-8'>
                 <div className='flex items-center justify-between gap-x-4'>
-                  <h3 id={planId} className='text-gray-900 text-lg font-semibold leading-8 dark:text-white'>
+                  <h3 id={planId} className='text-center text-gray-900 text-2xl font-semibold leading-8 dark:text-white'>
                     {paymentPlanCards[planId].name}
                   </h3>
                 </div>
-                <p className='mt-4 text-sm leading-6 text-gray-600 dark:text-white'>
+                <p className='font-bold mt-4 text-3xl text-gray-600 dark:text-white'>
                   {paymentPlanCards[planId].description}
                 </p>
                 <p className='mt-6 flex items-baseline gap-x-1 dark:text-white'>
@@ -126,13 +120,13 @@ const PricingPage = () => {
                     {paymentPlanCards[planId].price}
                   </span>
                   <span className='text-sm font-semibold leading-6 text-gray-600 dark:text-white'>
-                    {paymentPlans[planId].effect.kind === 'subscription' && '/month'}
+                    {paymentPlanCards[planId].name != PaymentPlanId.Yearly && '/yr'}
                   </span>
                 </p>
                 <ul role='list' className='mt-8 space-y-3 text-sm leading-6 text-gray-600 dark:text-white'>
                   {paymentPlanCards[planId].features.map((feature) => (
                     <li key={feature} className='flex gap-x-3'>
-                      <AiFillCheckCircle className='h-6 w-5 flex-none text-yellow-500' aria-hidden='true' />
+                      <AiFillCheckCircle className='h-6 w-5 flex-none text-raffleleader' aria-hidden='true' />
                       {feature}
                     </li>
                   ))}
@@ -145,9 +139,9 @@ const PricingPage = () => {
                   className={cn(
                     'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-yellow-400',
                     {
-                      'bg-yellow-500 text-white hover:text-white shadow-sm hover:bg-yellow-400':
+                      'bg-raffleleader text-white hover:text-white shadow-sm hover:bg-raffleleader':
                         planId === bestDealPaymentPlanId,
-                      'text-gray-600 ring-1 ring-inset ring-purple-200 hover:ring-purple-400':
+                      'text-gray-600 ring-1 ring-inset ring-raffleleader hover:ring-raffleleader':
                         planId !== bestDealPaymentPlanId,
                     }
                   )}
@@ -160,15 +154,15 @@ const PricingPage = () => {
                   aria-describedby={planId}
                   className={cn(
                     {
-                      'bg-yellow-500 text-white hover:text-white shadow-sm hover:bg-yellow-400':
+                      'bg-raffleleader text-white hover:text-white shadow-sm hover:bg-raffleleader':
                         planId === bestDealPaymentPlanId,
-                      'text-gray-600  ring-1 ring-inset ring-purple-200 hover:ring-purple-400':
+                      'text-gray-600  ring-1 ring-inset ring-raffleleader hover:text-white hover:bg-raffleleader':
                         planId !== bestDealPaymentPlanId,
                     },
                     {
                       'opacity-50 cursor-wait cursor-not-allowed': isStripePaymentLoading === planId,
                     },
-                    'mt-8 block rounded-md py-2 px-3 text-center text-sm dark:text-white font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-yellow-400'
+                    'mt-8 block rounded-md py-2 px-3 text-center text-sm dark:text-white font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-raffleleader'
                   )}
                 >
                   {!!user ? 'Buy plan' : 'Log in to buy plan'}
