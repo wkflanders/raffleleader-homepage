c0c7f43eec758a9af6d6211cbedf271b4d85f663
wkflanders <wflan@seas.upenn.edu>
2024-08-23 11:18:39 -0400

    howitworks styling

--- app/src/landing-page/components/HowItWorks.tsx
+++ app/src/landing-page/components/HowItWorks.tsx
@@ -73,21 +73,21 @@ const HowItWorksComponent: React.FC<HowItWorksProps> = ({ onCompletion }) => {
   return (
     <div ref={componentRef} className="md:py-30 py-10 text-center text-white">
       <h2 className="font-overpass text-xl font-light text-white sm:text-3xl lg:text-5xl mb-5 md:mb-15">How It Works</h2>
-      <div className="flex flex-col md:flex-row justify-between items-stretch mx-auto gap-5 md:gap-10 px-5" style={{ maxWidth: '1200px' }}>
-        <div ref={el => boxRefs.current[0] = el} className="fill-container flex flex-col justify-start items-center mx-auto bg-translucent text-white p-5 md:pt-25 rounded-3xl border border-white w-full md:w-1/3 h-80" style={{ flex: 1 }}>
+      <div className="flex flex-col md:flex-row justify-between items-stretch gap-5 md:gap-10 w-full px-60">
+        <div ref={el => boxRefs.current[0] = el} className="fill-container flex flex-col justify-start items-center mx-auto bg-translucent text-white p-5 md:pt-30 rounded-3xl border border-white md:w-1/3" style={{ flex: 1 }}>
           <div className="fill-animation"></div>
           <h3 className="md:text-3xl text-lg text-left font-semibold text-content md:mb-4">Create a Giveaway</h3>
-          <p className="lg:text-md md:text-md text-sm text-left text-content">Pick a template and customize it exactly to your liking.</p>
+          <p className="lg:text-lg md:text-md text-sm text-left text-content">Pick a template and customize it exactly to your liking.</p>
         </div>
-        <div ref={el => boxRefs.current[1] = el} className="fill-container flex flex-col justify-start items-center mx-auto bg-translucent text-white p-5 md:pt-25 rounded-3xl border border-white w-full md:w-1/3 h-80" style={{ flex: 1 }}>
+        <div ref={el => boxRefs.current[1] = el} className="fill-container flex flex-col justify-start items-center mx-auto bg-translucent text-white p-5 md:pt-30 rounded-3xl border border-white md:w-1/3" style={{ flex: 1 }}>
           <div className="fill-animation"></div>
           <h3 className="md:text-3xl text-lg text-left font-semibold text-content md:mb-4">Share Your Giveaway</h3>
-          <p className="lg:text-md md:text-md text-sm text-left text-content">Post your giveaway on your website and social media pages and immediately start gaining new leads and followers.</p>
+          <p className="lg:text-lg md:text-md text-sm text-left text-content">Post your giveaway on your website and social media pages and immediately start gaining new leads and followers.</p>
         </div>
-        <div ref={el => boxRefs.current[2] = el} className="fill-container flex flex-col justify-start items-center mx-auto bg-translucent text-white p-5 md:pt-25 rounded-3xl border border-white w-full md:w-1/3 h-80" style={{ flex: 1 }}>
+        <div ref={el => boxRefs.current[2] = el} className="fill-container flex flex-col justify-start items-center mx-auto bg-translucent text-white p-5 md:pt-30 rounded-3xl border border-white md:w-1/3" style={{ flex: 1 }}>
           <div className="fill-animation"></div>
           <h3 className="md:text-3xl text-lg text-left font-semibold text-content md:mb-4">Analyze Your Results</h3>
-          <p className="lg:text-md md:text-md text-sm text-left text-content">Raffle Leader's AI tools help you pick a winner and sift through your new leads.</p>
+          <p className="lg:text-lg md:text-md text-sm text-left text-content">Raffle Leader's AI tools help you pick a winner and sift through your new leads.</p>
         </div>
       </div>
     </div>
