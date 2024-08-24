c121142c4b9a5424e6a842b6540816b2a6f85e49
wkflanders <wflan@seas.upenn.edu>
2024-08-24 00:21:35 -0400

    login page component

--- app/src/auth/LoginPage.tsx
+++ app/src/auth/LoginPage.tsx
@@ -1,27 +1,40 @@
 import { Link } from 'react-router-dom';
 import { LoginForm } from 'wasp/client/auth';
 import { AuthPageLayout } from './AuthPageLayout';
+import type {CustomizationOptions} from "wasp/client/auth";
+import Logo from '../client/static/BANNER-LOGO.svg';
 
 export default function Login() {
+
+  const AuthAppearance: CustomizationOptions['appearance'] = {
+    colors: {
+      brand: '#1501FE',
+      brandAccent: '#1605E1',
+      submitButtonText: 'white',
+    }
+  }
+
   return (
     <AuthPageLayout>
-      <LoginForm />
+      <LoginForm 
+      appearance={AuthAppearance}
+      logo={Logo}
+      />
       <br />
       <span className='text-sm font-medium text-gray-900 dark:text-gray-900'>
         Don't have an account yet?{' '}
-        <Link to='/signup' className='underline'>
-          go to signup
+        <Link to='/pricing' className='underline'>
+          Get now!
         </Link>
-        .
       </span>
       <br />
-      <span className='text-sm font-medium text-gray-900'>
+      {/* <span className='text-sm font-medium text-gray-900'>
         Forgot your password?{' '}
         <Link to='/request-password-reset' className='underline'>
           reset it
         </Link>
         .
-      </span>
+      </span> */}
     </AuthPageLayout>
   );
 }
