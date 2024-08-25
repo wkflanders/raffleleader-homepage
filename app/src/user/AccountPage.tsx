c80b550298ce58df0136e05abe190ba3086922b2
wkflanders <wflan@seas.upenn.edu>
2024-08-24 22:27:45 -0400

    fix styling for account details page

--- app/src/user/AccountPage.tsx
+++ app/src/user/AccountPage.tsx
@@ -10,8 +10,8 @@ import { z } from 'zod';
 
 export default function AccountPage({ user }: { user: User }) {
   return (
-    <div className='mt-10 px-6'>
-      <div className='overflow-hidden border border-gray-900/10 shadow-lg sm:rounded-lg mb-4 lg:m-8 dark:border-gray-100/10'>
+    <div className='overflow-hidden mt-10 px-6'>
+      <div className=' border border-gray-900/10 shadow-lg sm:rounded-lg mb-4 lg:m-8 dark:border-gray-100/10'>
         <div className='px-4 py-5 sm:px-6 lg:px-8'>
           <h3 className='text-base font-semibold leading-6 text-gray-900 dark:text-white'>Account Information</h3>
         </div>
@@ -39,7 +39,7 @@ export default function AccountPage({ user }: { user: User }) {
                  />
             </div>
             <div className='py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6'>
-              <dt className='text-sm font-medium text-gray-500 dark:text-white'>About</dt>
+              <dt className='text-sm font-medium text-gray-500 dark:text-white'>Your License Key</dt>
               <dd className='mt-1 text-sm text-gray-900 dark:text-gray-400 sm:col-span-2 sm:mt-0'>
                 I'm a cool customer.
               </dd>
@@ -50,9 +50,9 @@ export default function AccountPage({ user }: { user: User }) {
       <div className='inline-flex w-full justify-end'>
         <button
           onClick={logout}
-          className='inline-flex justify-center mx-8 py-2 px-4 border border-transparent shadow-md text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          className='inline-flex justify-center mx-8 py-2 px-4 border border-transparent shadow-md text-sm font-medium rounded-md text-white bg-raffleleader hover:bg-rldark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
         >
-          logout
+          Logout
         </button>
       </div>
     </div>
