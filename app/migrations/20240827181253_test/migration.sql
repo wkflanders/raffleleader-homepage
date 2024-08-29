b10907ea590c0f411de28c4d30743c748b449d81
William Flanders <wflan@seas.upenn.edu>
2024-08-28 20:06:27 -0400

    db migrations post schema change

--- app/migrations/20240827181253_test/migration.sql
+++ app/migrations/20240827181253_test/migration.sql
@@ -129,9 +129,6 @@ CREATE TABLE "Session" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
