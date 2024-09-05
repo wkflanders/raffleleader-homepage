a5dd9580cf0ce1acdf0348570615ace095b9c9fa
William Flanders <wflan@seas.upenn.edu>
2024-09-05 09:22:36 -0400

    stripe versioning fix

--- app/src/payment/stripe/stripeClient.ts
+++ app/src/payment/stripe/stripeClient.ts
@@ -7,5 +7,5 @@ export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   // npm package to the API version that matches your Stripe dashboard's one.
   // For more details and alternative setups check 
   // https://docs.stripe.com/api/versioning .
-  apiVersion: '2022-11-15',
+  apiVersion: '2023-10-16',
 });
