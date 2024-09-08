45f1cdae7776730e4d7947d18bb9d59391ecd647
William Flanders <wflan@seas.upenn.edu>
2024-09-08 18:36:56 -0400

    navbar, closing, and footer updates

--- app/src/landing-page/LandingPage.tsx
+++ app/src/landing-page/LandingPage.tsx
@@ -3,7 +3,6 @@ import {
   features,
   navigation,
   faqs,
-  footerNavigation,
   stats
 } from './contentSections';
 import Header from './components/Header';
@@ -17,9 +16,13 @@ import Footer from './components/Footer';
 export default function LandingPage() {
   const [howItWorksCompleted, setHowItWorksCompleted] = useState(false);
 
+  const handlePricingClick = () => {
+    setHowItWorksCompleted(true);
+  };
+
   return (
-    <div className='bg-white dark:text-white dark:bg-boxdark-2 no-scrollbar overflow-y-auto'>
-      <Header navigation={navigation} />
+    <div className='bg-white dark:bg-boxdark-2 no-scrollbar '>
+      <Header navigation={navigation} onPricingClick={handlePricingClick} />
 
       <main className='isolate dark:bg-boxdark-2 overflow-x-hidden'>
         <Hero />
@@ -38,20 +41,19 @@ export default function LandingPage() {
             <FAQ faqs={faqs}/>
 
             {/* Closing */}
-            <div className="bg-rl_wave bg-top bg-cover bg-no-repeat text-white text-center py-60 rounded-lg">
-              <h2 className="leading-tight lg:leading-loose text-5xl lg:text-7xl font-normal mb-8 lg:mb-0">Try <span className="font-semibold tracking-widest">Raffle Leader</span></h2>
-              <p className="leading-tight lg:leading-loose text-3xl lg:text-5xl mb-8 lg:mb-4">Risk Free For 14 Days</p>
-              <p className="leading-8 lg:leading-loose text-2xl tracking-wider mb-8 lg:mb-4">And spend more time converting leads, not finding them.</p>
+            <div className="bg-rl_wave bg-top bg-cover bg-no-repeat text-white text-center py-30 rounded-lg -mb-2 -mx-2">
+              <h2 className="text-5xl lg:text-7xl font-normal mb-6" style={{ lineHeight: 1.2 }}>Stop Wasting Time <br></br> Chasing Customers</h2>
+              <p className="text-3xl tracking-wider" style={{ lineHeight: 1.2 }}>Let Raffle Leader bring them to you</p>
               <div className='mt-10 flex items-center justify-center gap-x-6'>
                 <a
                   href={'/pricing'}
                   className='rounded-lg px-17 py-4 text-xl font-semibold bg-white text-raffleleader ring-1 ring-inset ring-gray-200 hover:ring-2 hover:ring-gray-200 hover:bg-gray-200 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:text-white'
                 >
-                  Get Started
+                  Get Raffle Leader
                 </a>
               </div>
             </div>
-            <Footer footerNavigation={footerNavigation} />
+            <Footer />
           </>
         )}
       </main>
